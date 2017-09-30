340ef2b0da3a58b31b4ffec9b09098fff57a00e1
Dongyoung_Kim <521ab@naver.com>
2017-09-30 16:53:50 +0900

    IOT Core Blink 듀티값 주기

--- IOTCore_Blink/MainPage.xaml.cs
+++ IOTCore_Blink/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Gpio;
+using System.Threading.Tasks;
 
 // 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x412에 나와 있습니다.
 
@@ -22,9 +24,60 @@ namespace IOTCore_Blink
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const byte ledPin = 5;
+        private GpioPin pin;
+
+        private UInt16 duty = 1000; 
+        
         public MainPage()
         {
             this.InitializeComponent();
+
+            setup();
+
+            loop();
         }
+
+        public void setup()
+        {
+            var gpio = GpioController.GetDefault();
+
+            if (gpio == null)
+            {
+                pin = null;
+                return;
+            }
+
+            pin = gpio.OpenPin(ledPin);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+            pin.Write(GpioPinValue.High);
+        }
+
+        public async void loop()
+        {
+            while (true)
+            {
+                pin.Write(GpioPinValue.Low);
+                await Task.Delay(duty);
+                pin.Write(GpioPinValue.High);
+                await Task.Delay(duty);
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            duty = UInt16.Parse( DutyBox.Text);
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    pin.Write(GpioPinValue.Low);
+        //}
+
+        //private void Button_Click_1(object sender, RoutedEventArgs e)
+        //{
+        //    pin.Write(GpioPinValue.High);
+        //}
     }
 }
